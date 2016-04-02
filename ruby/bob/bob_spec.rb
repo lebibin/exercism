629771989ae2ebc67f8b9d16b062782b02512114
Kevin Candelaria <kevdashdev@gmail.com>
2016-04-03 00:19:44 +0800

    Break down bob remarks instead of one big tests array for bob spec

--- ruby/bob/bob_spec.rb
+++ ruby/bob/bob_spec.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-
 gem 'rspec'
 
 require 'rspec/core'
@@ -10,96 +9,68 @@ CHILL = 'Whoa, chill out!'
 SURE = 'Sure.'
 FINE = 'Fine. Be that way!'
 
-TESTS = [
-  {
-    remark:  'Tom-ay-to, tom-aaaah-to.',
-    reply: WHATEVER
-  },
-  {
-    remark:  'WATCH OUT!',
-    reply: CHILL
-  },
-  {
-    remark:  ('A'..'Z').to_a.sample(10).join,
-    reply: CHILL
-  },
-  {
-    remark:  'Does this cryogenic chamber make me look fat?',
-    reply: SURE
-  },
-  {
-    remark:  'You are, what, like 15?',
-    reply: SURE
-  },
-  {
-    remark:('a'..'z').to_a.sample(10).join << '?',
-    reply: SURE
-  },
-  {
-    remark:  'Let\'s go make out behind the gym!',
-    reply: WHATEVER
-  },
-  {
-    remark:  'It\'s OK if you don\'t want to go to the DMV.',
-    reply: WHATEVER
-  },
-  {
-    remark:  'WHAT THE HELL WERE YOU THINKING?',
-    reply: CHILL
-  },
-  {
-    remark:  '1, 2, 3 GO!',
-    reply: CHILL
-  },
-  {
-    remark:  '1, 2, 3',
-    reply: WHATEVER
-  },
-  {
-    remark:  '4?',
-    reply: SURE
-  },
-  {
-    remark:  'ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!)',
-    reply: CHILL
-  },
-  {
-    remark:  'I HATE YOU',
-    reply: CHILL
-  },
-  {
-    remark:  'Ending with ? means a question.',
-    reply: WHATEVER
-  },
-  {
-    remark:  'Wait! Hang on. Are you going to be OK?',
-    reply: SURE
-  },
-  {
-    remark:  '',
-    reply: FINE
-  },
-  {
-    remark:  ' ' * rand(1..10),
-    reply: FINE
-  },
-  {
-    remark: "\t" * rand(1..10),
-    reply: FINE
-  },
-  {
-    remark:  """
+WHATEVER_REMARKS = [
+  'Tom-ay-to, tom-aaaah-to.',
+  'Let\'s go make out behind the gym!',
+  'It\'s OK if you don\'t want to go to the DMV.',
+  '1, 2, 3',
+  'Ending with ? means a question.',
+  """
     Does this cryogenic chamber make me look fat?
     no
-    """,
-      reply: WHATEVER
-  },
+  """
+]
+
+CHILL_REMARKS = [
+  'WATCH OUT!',
+  ('A'..'Z').to_a.sample(10).join,
+  'WHAT THE HELL WERE YOU THINKING?',
+  '1, 2, 3 GO!',
+  'ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!)',
+  'I HATE YOU',
+]
+
+SURE_REMARKS = [
+  'Does this cryogenic chamber make me look fat?',
+  'You are, what, like 15?',
+  ('a'..'z').to_a.sample(10).join << '?',
+  '4?',
+  'Wait! Hang on. Are you going to be OK?',
+]
+
+FINE_REMARKS = [
+  '',
+  ' ' * rand(1..10),
+  "\t" * rand(1..10),
 ]
 
 RSpec.describe Bob do
-  TESTS.each do |test|
-    it "replies to '#{test[:remark]}' accordingly with '#{test[:reply]}'" do
-      expect(subject.hey(test[:remark])).to eql(test[:reply])
+  context "reply: #{WHATEVER.inspect}" do
+    WHATEVER_REMARKS.each do |remark|
+      it "remark: #{remark.inspect}" do
+        expect(subject.hey(remark)).to eql(WHATEVER)
+      end
+    end
+  end
+  context "reply: #{CHILL.inspect}" do
+    CHILL_REMARKS.each do |remark|
+      it "remark: #{remark.inspect} with #{CHILL.inspect}" do
+        expect(subject.hey(remark)).to eql(CHILL)
+      end
+    end
+  end
+  context "reply: #{SURE.inspect}" do
+    SURE_REMARKS.each do |remark|
+      it "remark: #{remark.inspect} with #{SURE.inspect}" do
+        expect(subject.hey(remark)).to eql(SURE)
+      end
+    end
+  end
+  context "reply: #{FINE.inspect}" do
+    FINE_REMARKS.each do |remark|
+      it "remark: #{remark.inspect} with #{FINE.inspect}" do
+        expect(subject.hey(remark)).to eql(FINE)
+      end
     end
   end
 end
