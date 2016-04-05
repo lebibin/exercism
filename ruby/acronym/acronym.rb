fb2187f1646d52d5387920d8bf8eaeaaf38055b0
Kevin Candelaria <kevdashdev@gmail.com>
2016-04-05 20:26:34 +0800

    Refactor acronym class to use #tap, constants, and private methods

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,12 +1,19 @@
 class Acronym
   VERSION = 1.0
+  WORD_REGEX = /([A-Z]?[a-z]+|[A-Z]+)\s*/
+  NON_WORD_REGEX = /(\W|\A\z)/
+  
 
   def self.abbreviate(word)
-    acronym = ""
-    split = word.split(/([A-Z]?[a-z]+|[A-Z]+)\s*/).reject{|w| w =~ /\W/ || w.empty?}
-    split.each do |w|
-      acronym += w[0]
-    end
-    acronym.upcase
+    "".tap do |acronym|
+      split_into_words(word).each do |acronym_word|
+        acronym << acronym_word[0]
+      end
+    end.upcase
+  end
+
+  private
+  def self.split_into_words(word)
+    word.split(WORD_REGEX).reject{|w| w =~ NON_WORD_REGEX }
   end
 end
